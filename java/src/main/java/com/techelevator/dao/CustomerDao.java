8257b3cc8daa7d6656f64b67d899635424fffcb9
Matthew Burtonshaw <MBurtonshaw@gmail.com>
2024-08-05 09:59:02 -0400

    MB

---
+++ java/src/main/java/com/techelevator/dao/CustomerDao.java
@@ -0,0 +1,30 @@
+//package com.techelevator.dao;
+//
+//import com.techelevator.model.Customer;
+//import com.techelevator.model.RegisterUserDto;
+//import com.techelevator.model.User;
+//
+//import java.util.List;
+//
+//public abstract class CustomerDao {
+//    public abstract Customer getCustomerById(int userId);
+//
+//    public abstract Customer getCustomerById(int userId);
+//
+//    public abstract List<Customer> getCustomers();
+//
+//    public abstract List<Customer> getCustomers();
+//
+//    public abstract User getUserByUsername(String username);
+//
+//    public abstract User createUser(RegisterUserDto user);
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
