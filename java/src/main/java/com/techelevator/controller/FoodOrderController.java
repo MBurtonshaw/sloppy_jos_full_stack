0e7c38492b989f70ff137ac3e903388b563deb17
Matthew Burtonshaw <MBurtonshaw@gmail.com>
2024-08-05 14:05:21 -0400

    MB

--- java/src/main/java/com/techelevator/controller/FoodOrderController.java
+++ java/src/main/java/com/techelevator/controller/FoodOrderController.java
@@ -27,36 +27,31 @@ public class FoodOrderController {
     private final String API_BASE_URL = "http://localhost:9000/";
 
 
-    public FoodOrderController(FoodOrderDao foodOrderDao, FoodOrderService foodOrderService) {
-        this.foodOrderDao = foodOrderDao;
-        this.foodOrderService = foodOrderService;
-
     public FoodOrderController(FoodOrderDao foodOrderDao) {
-
         this.foodOrderDao = foodOrderDao;
-
-
-    @RequestMapping(path = "/menu/pizzas", method = RequestMethod.GET)
-    public List<Food> getPizzas() {
-        try {
-            return foodOrderDao.getPizzas();
-        } catch (DaoException e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to retrieve pizzas", e);
-        }
     }
+
+//    @RequestMapping(path = "/menu/pizzas", method = RequestMethod.GET)
+//    public List<Food> getPizzas() {
+//        try {
+//            return foodOrderDao.getPizzas();
+//        } catch (DaoException e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to retrieve pizzas", e);
+//        }
+//    }
 
     // MAY HAVE TO USE QUERY PARAMETERS INSTEAD
-    @RequestMapping(path = "/menu/pizzas/{id}", method = RequestMethod.GET)
-    public Food getPizza(@PathVariable int id) {
-        try {
-            return foodOrderDao.getPizza(id);
-        } catch (DaoException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza not found", e);
-        }
-    }
+//    @RequestMapping(path = "/menu/pizzas/{id}", method = RequestMethod.GET)
+//    public Food getPizza(@PathVariable int id) {
+//        try {
+//            return foodOrderDao.getPizza(id);
+//        } catch (DaoException e) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza not found", e);
+//        }
+//    }
 
     @RequestMapping(path = "/menu/specialty_pizzas", method = RequestMethod.GET)
-    public List<Food> getSpecialtyPizzas() {
+    public List<SpecialtyPizza> getSpecialtyPizzas() {
         try {
             return foodOrderDao.getSpecialtyPizzas();
         } catch (DaoException e) {
@@ -64,54 +59,50 @@ public class FoodOrderController {
         }
     }
 
-    @RequestMapping(path = "/menu/specialty_pizzas/{id}", method = RequestMethod.GET)
-    public Food getSpecialtyPizza(@PathVariable int id) {
-        try {
-            return foodOrderDao.getSpecialtyPizza(id);
-        } catch (DaoException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Specialty pizza not found", e);
-        }
-    }
+//    @RequestMapping(path = "/menu/specialty_pizzas/{id}", method = RequestMethod.GET)
+//    public Food getSpecialtyPizza(@PathVariable int id) {
+//        try {
+//            return foodOrderDao.getSpecialtyPizza(id);
+//        } catch (DaoException e) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Specialty pizza not found", e);
+//        }
+//    }
 
-    @RequestMapping(path = "/menu/sides", method = RequestMethod.GET)
-    public List<Food> getSides() {
-        try {
-            return foodOrderDao.getSides();
-        } catch (DaoException e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to retrieve sides", e);
-        }
-    }
-
-    @RequestMapping(path = "/menu/sides/{id}", method = RequestMethod.GET)
-    public Food getSide(@PathVariable int id) {
-        try {
-            return foodOrderDao.getSide(id);
-        } catch (DaoException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Side not found", e);
-        }
-    }
-
-
-    @RequestMapping(path = "/order/{id}", method = RequestMethod.GET)
-    public Order getOrder(@PathVariable int id) {
+//    @RequestMapping(path = "/menu/sides", method = RequestMethod.GET)
+//    public List<Food> getSides() {
+//        try {
+//            return foodOrderDao.getSides();
+//        } catch (DaoException e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to retrieve sides", e);
+//        }
+//    }
+//
+//    @RequestMapping(path = "/menu/sides/{id}", method = RequestMethod.GET)
+//    public Food getSide(@PathVariable int id) {
+//        try {
+//            return foodOrderDao.getSide(id);
+//        } catch (DaoException e) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Side not found", e);
+//        }
+//    }
 
 
-    @RequestMapping(path = "/order/{id}", method = RequestMethod.GET)
-    public FoodOrder getOrder(@PathVariable int id) {
+//    @RequestMapping(path = "/order/{id}", method = RequestMethod.GET)
+//    public FoodOrder getOrder(@PathVariable int id) {
+//
+//        try {
+//            return foodOrderDao.getOrder(id);
+//        } catch (DaoException e) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Order not found", e);
+//        }
+//    }
 
-        try {
-            return foodOrderDao.getOrder(id);
-        } catch (DaoException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Order not found", e);
-        }
-    }
-
-    @RequestMapping(path = "/order/{id}/status", method = RequestMethod.GET)
-    public boolean getOrderStatus(@PathVariable int id) {
-        try {
-            return foodOrderDao.getOrderStatus(id);
-        } catch (DaoException e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to retrieve order status", e);
-        }
-    }
+//    @RequestMapping(path = "/order/{id}/status", method = RequestMethod.GET)
+//    public boolean getOrderStatus(@PathVariable int id) {
+//        try {
+//            return foodOrderDao.getOrderStatus(id);
+//        } catch (DaoException e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to retrieve order status", e);
+//        }
+//    }
 }
