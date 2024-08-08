36e98b6726b7ba0bafbe6d0bd2cfc6377c576961
Matthew Armour <mkarmour@protonmail.com>
2024-08-08 10:22:04 -0400

    TisForToppings

---
+++ java/src/main/java/com/techelevator/dao/FoodOrderDao.java
@@ -0,0 +1,58 @@
+
+package com.techelevator.dao;
+
+
+
+import com.techelevator.model.Food;
+import com.techelevator.model.Item;
+import com.techelevator.model.SpecialtyPizza;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FoodOrderDao is an interface that defines methods for
+ * accessing and managing food items in a database or collection.
+ */
+
+public interface FoodOrderDao {
+    List<SpecialtyPizza> getSpecialtyPizzas();
+    SpecialtyPizza getSpecialtyPizza(int id);
+    Item addPizza(Item pizza);
+//SpecialtyPizza addSpecialtyPizza(int id);
+
+    /**
+     * Retrieves all specialty pizzas from the data source.
+     * @return a list of Food objects representing specialty pizzas.
+     */
+
+
+    /**
+     * Retrieves a pizza by its unique identifier.
+     * @param id The unique identifier of the pizza.
+     * @return a Food object representing the pizza.
+     */
+    Food getPizzaById(int id);
+
+    /**
+     * Adds a new pizza to the data source.
+     * @param pizza The Food object representing the new pizza to add.
+     */
+    void addPizza(Food pizza);
+
+    /**
+     * Updates an existing pizza in the data source.
+     * @param pizza The Food object representing the pizza to update.
+     */
+    void updatePizza(Food pizza);
+
+    /**
+     * Deletes a pizza from the data source by its unique identifier.
+     * @param id The unique identifier of the pizza to delete.
+     */
+    void deletePizza(int id);
+}
